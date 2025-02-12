d86b7f5090f97452ed4bf4190ef93b2b976ac2d3
Luis Pires <luispires310500@gmail.com>
2025-02-12 16:04:50 +0000

    Implementing solution in index.js file

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+let bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const dns = require('node:dns');
+const { error } = require('node:console');
+
+var shortedURLs = {};
+var id = 1;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +23,33 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+//const hostname = ;
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/api/shorturl', function(req, res) {
+  //var urlToShorten = req.body.url.slice(7);
+  console.log(dns.lookup(new URL(req.body.url).hostname,(err,address,family)=>{
+    console.log(`${address} \n ${req.body.url}\n`);
+    if(err){
+      res.json({ error: 'invalid url' });
+    }else{
+      shortedURLs[id] = req.body.url;
+      res.json({original_url: req.body.url, short_url: id});
+      id++;
+    }
+  }));
+});
+
+app.get('/api/shorturl/:id?', function(req,res){
+  if(shortedURLs.hasOwnProperty(req.params.id)){
+    res.redirect(shortedURLs[req.params.id]);
+  } else{
+    console.log("ERROR");
+    res.json({error: 'No shorten URL found for given input'});
+  }
 });
 
 app.listen(port, function() {
